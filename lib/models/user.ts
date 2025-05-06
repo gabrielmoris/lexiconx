836c9a2292d96d5ead116af26349bb9949a09cb5
gabriel <gabrielcmoris@gmail.com>
2025-05-06 23:46:50 +0200

    feat: Add user authentication with NextAuth and Google provider

---
+++ lib/models/user.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    image: { type: String, required: true },
+  },
+  { _id: false }
+);
+
+const SessionSchema = new mongoose.Schema({
+  user: { type: UserSchema, required: true },
+  expires: { type: Date, required: true },
+});
+
+const Session = mongoose.models.Session || mongoose.model("Session", SessionSchema);
+
+export default Session;
