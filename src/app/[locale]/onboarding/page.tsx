4d295ac864b4a54ed7d3a6cf518ee679bdc7712c
gabriel <gabrielcmoris@gmail.com>
2025-07-03 22:27:30 +0200

    feat: implement user data fetching and redirection in WordList, add error message to translations, and rename Login component to Onboarding

--- src/app/[locale]/onboarding/page.tsx
+++ src/app/[locale]/onboarding/page.tsx
@@ -2,7 +2,7 @@
 
 // import { useLocale } from "next-intl";
 
-export default function Login() {
+export default function OnBorading() {
   //   const locale = useLocale();
 
   return <main className="min-h-screen flex flex-col items-center justify-center">onboarding</main>;
