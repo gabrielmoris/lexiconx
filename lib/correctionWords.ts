c875f1e7845c502a22461c325f2ce72990cca8be
gabriel <gabrielcmoris@gmail.com>
2026-04-27 22:51:32 +0200

    fix: standardize indentation in correctionWords.ts for improved readability

--- lib/correctionWords.ts
+++ lib/correctionWords.ts
@@ -2,27 +2,27 @@ import { Word } from "@/types/Words";
 import { getWordsByIds } from "./apis";
 
 export const failWords = async (wordsIds: string[]): Promise<Word[]> => {
-	const { data } = await getWordsByIds(wordsIds);
+  const { data } = await getWordsByIds(wordsIds);
 
-	data.forEach((word: Word) => {
-		word.nextReview = new Date().toISOString();
-		word.interval = 0;
-		word.repetitions = 0;
-		word.easeFactor = parseFloat((word.easeFactor - 0.1).toFixed(2));
-	});
+  data.forEach((word: Word) => {
+    word.nextReview = new Date().toISOString();
+    word.interval = 0;
+    word.repetitions = 0;
+    word.easeFactor = parseFloat((word.easeFactor - 0.1).toFixed(2));
+  });
 
-	return data;
+  return data;
 };
 
 export const successWords = async (wordsIds: string[]): Promise<Word[]> => {
-	const { data } = await getWordsByIds(wordsIds);
+  const { data } = await getWordsByIds(wordsIds);
 
-	data.forEach((word: Word) => {
-		word.nextReview = new Date().toISOString();
-		word.interval = word.interval > 5 ? word.interval + 1 : word.interval;
-		word.repetitions = word.repetitions + 1;
-		word.easeFactor = parseFloat((word.easeFactor + 0.1).toFixed(2));
-	});
+  data.forEach((word: Word) => {
+    word.nextReview = new Date().toISOString();
+    word.interval = word.interval > 5 ? word.interval + 1 : word.interval;
+    word.repetitions = word.repetitions + 1;
+    word.easeFactor = parseFloat((word.easeFactor + 0.1).toFixed(2));
+  });
 
-	return data;
+  return data;
 };
