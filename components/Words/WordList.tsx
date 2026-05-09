7c44d46b53225285fed0de2f3501fc2eee526f9e
gabriel <gabrielcmoris@gmail.com>
2026-05-09 20:05:11 +0200

    refactor: standardize quotes and improve conditional checks in WordList and WordsContext

--- components/Words/WordList.tsx
+++ components/Words/WordList.tsx
@@ -10,12 +10,15 @@ const WordList = () => {
   const { isSelectedLanguageLoading } = useLanguage();
   const t = useTranslations('word-list');
   const { loading, words } = useWords();
+
   if (loading) {
     return <LoadingComponent />;
   }
+
   if ((!words || words.length === 0) && isSelectedLanguageLoading === false) {
     return <div className="text-theme-text-light dark:text-theme-text-dark">{t('no-words')}</div>;
   }
+
   return (
     <section className="w-full grid grid-cols-1 md:grid-cols-3 gap-5">
       {words && words.map(word => <WordCard key={word?._id || word.word} word={word} />)}
